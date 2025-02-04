8f3a7b2779b900bd2e6bd53311427d71fa6cc986
john michael borromeo <jmgamer345@gmail.com>
2025-02-04 15:55:31 +0800

    hahays

---
+++ MVVM/Models/FlashCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards.MVVM.Models
+{
+    public class FlashCard
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        public FlashCard(string question, string answer)
+        {
+            Question = question;
+            Answer = answer;
+        }
+    }
+}
---
+++ MVVM/ViewModels/FlashcardViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flashcards.MVVM.Models;
+
+namespace Flashcards.MVVM.ViewModels
+{
+    public partial class FlashcardViewModel
+    {
+        private ObservableCollection<Models.FlashCard> _flashcards;
+
+        private int _currentIndex;
+
+        private Models.FlashCard _currentFlashcard;
+
+        private bool _isAnswerVisible;
+
+        public FlashcardViewModel()
+        {
+            _flashcards = new ObservableCollection<Models.FlashCard>
+            {
+                new Models.FlashCard("Whats my name", "Jm"),
+            };
+
+            
+        }
+    }
+}
